2d6e0bdb07d8740ae39d98cf6be9b73c25c6164b
Angel Ho <66530174+Angel-Ho221@users.noreply.github.com>
2020-07-28 14:01:14 -0400

    Update typewriter2.js

--- d2/typewriter2.js
+++ d2/typewriter2.js
@@ -5,8 +5,7 @@ let startTime = 1000;
 const typewriter = (sentence) => {
   let startTime = 100;
   for (const char in sentence) {
-    setTimeout(() => {process.stdout.write(sentence[char]) // print the letters in one line
-      // console.log(sentence[char])// prints each letter one at a time
+    setTimeout(() => {process.stdout.write(sentence[char]) 
     }, (startTime + 100 * char));
   }
 }
